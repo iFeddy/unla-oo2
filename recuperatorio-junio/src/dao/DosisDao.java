9dca61dabfa20bdf7bcd2eb44eec0ae697fa88fa
Federico Perez <feddy_7990@hotmail.com>
2021-07-14 14:38:37 -0300

    Agregado Lazy en True (Proxy)

--- recuperatorio-junio/src/dao/DosisDao.java
+++ recuperatorio-junio/src/dao/DosisDao.java
@@ -3,6 +3,7 @@ package dao;
 import java.time.LocalTime;
 import java.util.List;
 
+import org.hibernate.Hibernate;
 import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
@@ -38,6 +39,10 @@ public class DosisDao {
 		try {
 			iniciaOperacion();
 			objeto = session.createQuery("from Dosis d where d.vacuna =" + vacuna.getIdVacuna()).list();
+			for (Dosis d : objeto) {
+				Hibernate.initialize(d.getPersona());
+				Hibernate.initialize(d.getVacuna());
+			}
 		} finally {
 			session.close();
 		}
@@ -52,12 +57,19 @@ public class DosisDao {
 			objeto = session
 					.createQuery("from Dosis d where d.hora BETWEEN '" + horaDesde + "' AND '" + horaHasta + "'")
 					.list();
+			for (Dosis d : objeto) {
+				Hibernate.initialize(d.getPersona());
+				Hibernate.initialize(d.getVacuna());
+			}
 		} finally {
 			session.close();
 		}
 		return objeto;
 	}
 
+	//Observacion de Romina: Quiere que use el mismo metodo que los casos de usos anteriores para no repetir la query (?)
+	//Ni idea para mi son querys distintas, habira que poner una bandera en string "all", "true", "false" para comorbilidades
+	//Pero dejaria de ser con los parametros que piden.
 	@SuppressWarnings("unchecked")
 	public List<Dosis> traerDosis(LocalTime horaDesde, LocalTime horaHasta, boolean tieneComorbilidades) {
 		List<Dosis> objeto = null;
@@ -65,6 +77,10 @@ public class DosisDao {
 			iniciaOperacion();
 			objeto = session.createQuery("from Dosis d inner join fetch d.persona p where d.hora BETWEEN '" + horaDesde
 					+ "' AND '" + horaHasta + "' AND p.tieneComorbilidad = " + tieneComorbilidades).list();
+			for (Dosis d : objeto) {
+				Hibernate.initialize(d.getPersona());
+				Hibernate.initialize(d.getVacuna());
+			}
 		} finally {
 			session.close();
 		}
